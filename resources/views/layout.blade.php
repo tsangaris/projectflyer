51aaf46983328804b3b7e5c64300f8d2779585cb
tsangaris <christostsang@gmail.com>
2016-10-24 18:36:21 +0300

    Install Laravel

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+      <div class="container">
+        @yield('content')
+      </div>
+  </body>
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+
+@section('title')
+  Home
+@endsection
+
+@section('content')
+
+  <h1>Project Flyer</h1>
+
+@endsection
